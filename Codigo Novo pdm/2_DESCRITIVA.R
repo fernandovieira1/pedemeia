c6e975ee62b2ff3697b1808329257705e94815a3
Fernando <fernandovieira1@outlook.com>
2024-12-17 23:58:06 -0300

    descrição base evasão - windows 17122024

---
+++ Codigo Novo pdm/2_DESCRITIVA.R
@@ -0,0 +1,8 @@
+### DADOS PNAD -- Estatística Descritiva
+
+######################## 1. BASE EVASÃO ########################
+str(base_evasao_filtrada)
+names(base_evasao_filtrada)
+
+head(base_evasao_filtrada$V2007)
+
